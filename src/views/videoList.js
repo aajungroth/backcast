ffe393a4a348e29081f403f5e5f0f93a1ca0d089
Alex Jungroth <aajungroth@ucdavis.edu>
2017-06-01 17:58:24 +0000

    Almost renders app for local video files

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,16 +1,24 @@
 var VideoListView = Backbone.View.extend({
 
-
+  initialize: function() {
+    this.listenTo(this.collection, 'sync', this.render);
+  },
   render: function() {
-    this.$el.children().detach();
-    this.$el.html(this.template());
+    //this.$el.empty();
+    //this.collection.forEach(this.renderVideo, this);
     // var videoList = new VideoListEntryView({
     //   el: this.$('#videos'),
     //   collection: this.collection
     // });
     // _.each(videoList, VideoListEntryView.prototype.render);
+    this.$el.children().detach();
+    this.$el.html(this.template());
+    this.collection.forEach(this.renderVideo, this);
+  },
 
-    return this;
+  renderVideo: function(video) {
+    var videoListEntry = new VideoListEntryView({model: video});
+    this.$el.append(videoListEntry.render());
   },
 
   template: templateURL('src/templates/videoList.html')
--- src/views/videoPlayer.js
+++ src/views/videoPlayer.js
@@ -1,5 +1,8 @@
 var VideoPlayerView = Backbone.View.extend({
 
+  initialize: function() {
+    this.listenTo(this.collection, 'select', this.render);
+  },
 
   render: function() {
     this.$el.html('<div class="loading">Please wait...</div>');
