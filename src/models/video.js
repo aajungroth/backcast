829efb723356e2786a722a6d7c15673b1312bdca
Alex Jungroth <aajungroth@ucdavis.edu>
2017-06-04 03:53:33 +0000

    Videos now play, updated model to use the API key

--- src/models/video.js
+++ src/models/video.js
@@ -3,7 +3,7 @@ var Video = Backbone.Model.extend({
   initialize: function(video) {
     // override youtube's complex id field
     //this.set('id', video.id.videoId);
-    this.set('id', 'YOUTUBE_API_KEY');
+    this.set('id', video.id.videoId);
   },
 
   select: function() {
